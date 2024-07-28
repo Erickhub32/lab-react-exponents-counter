71a9fa77a91fa43a6c6c1e56d41f52b4082fdca6
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-07-28 18:05:35 +0200

    getting progress

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,6 +6,8 @@ const Counter = () => {
   const decrement = () => setCount((prevCount) => prevCount - 1);
   const increment = () => setCount((prevCount) => prevCount + 1);
 
+
+
   return (
     <div className="counter-container">
       <p className="counter-value">{count}</p>
